539cef85f30e3c5924deb2ac9c71b9d79886afb2
Anthony Buchholz <anthony.buchholz@gmail.com>
2016-09-06 13:59:27 -0500

    Created StuckInATimeLoop.cpp

---
+++ StuckInATimeLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    short numTimes;
+    cin >> numTimes;
+    for(int i = 1; i <= numTimes; i++){
+        cout << i << " Abracadabra" << endl;
+    }
+    return 0;
+}
