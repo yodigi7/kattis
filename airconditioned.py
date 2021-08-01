6471226b7038d1d963b327c9829dbddf27aa63c1
Anthony Buchholz <anthony.buchholz@gmail.com>
2021-08-01 01:12:40 -0500

    Wrong answer airconditioned, not sure why.

---
+++ airconditioned.py
@@ -0,0 +1,22 @@
+def solution(minions):
+    rooms = []
+    for minion in minions:
+        minion[1]+=1
+        minion_range = tuple(range(*minion))
+        for i, room in enumerate(rooms):
+            if not set(room).isdisjoint(minion_range):
+                rooms[i] = set(room).intersection(minion_range)
+                break
+        else:
+            rooms.append(minion_range)
+    return rooms
+
+
+def main():
+    minions = ([int(x) for x in input().split()] for _ in range(int(input())))
+    sol = solution(minions)
+    # print(sol)
+    print(len(sol))
+
+
+main()
