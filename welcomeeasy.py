9b488559fd1d2ca3623e4d1f3fa8f4f601b67267
Anthony Buchholz <anthony.buchholz@gmail.com>
2021-08-04 21:11:16 -0500

    welcomeeasy

--- welcomeeasy.py
+++ welcomeeasy.py
@@ -1,18 +1,17 @@
 def solve(sentence, sub_sentence):
     if len(sub_sentence) > len(sentence):
         return 0
+    for char in sub_sentence:
+        if char not in sentence:
+            return 0
     if len(sub_sentence) == 1:
         return sentence.count(sub_sentence)
     curr_char = sub_sentence[0]
     count = 0
-    next_sub_sentence = sub_sentence[1:]
     curr_sentence = sentence
     for _ in range(sentence.count(curr_char)):
         idx = curr_sentence.index(curr_char)
-        try:
-            count += solve(curr_sentence[idx+1:], next_sub_sentence)
-        except IndexError:
-            break
+        count += solve(curr_sentence[idx+1:], sub_sentence[1:])
         curr_sentence = curr_sentence[idx+1:]
     return count
 
